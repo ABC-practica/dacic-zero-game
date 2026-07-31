wejusdgfsJKA/DacicZeroGame

nu mi place viziunea autorului

Assets/_Project/Scripts/Weapons/LongBow.cs
@@ -1,3 +1,8 @@
+using EventBus;
+using HP;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Weapons
@@ -15,21 +20,91 @@ namespace Weapons
 
         protected override void Fire()
         {
+            IsEnhanced = true;
             animancer.Play(clip).Time = 0;
             cooldownTo = Time.time + fireCooldown;
-            ShootArrow(currentCharge);
-            ammo -= 1;
+            if (!IsEnhanced)
+            {
+                ShootArrow(currentCharge);
+                ammo -= 1;
+            }
+            else
+            {
+                const float MOONLIGHT_GREATSWORD_ZISE_AUTORUL = 3f;
+                ShootArrow(currentCharge);
+                ShootArrow(currentCharge, MOONLIGHT_GREATSWORD_ZISE_AUTORUL);
+                ShootArrow(currentCharge, (-1) * MOONLIGHT_GREATSWORD_ZISE_AUTORUL);
+                ammo -= 1;
+            }
         }
 
         protected override void AltFire()
         {
+            StartCoroutine(AltFireAction());
+            cooldownTo = Time.time + fireCooldown;
+        }
+
+        IEnumerator AltFireAction()
+        {
+            IsEnhanced = true;
             const float SphereRadius = 2f;
             const float SphereDistance = 0f;
-            const int SphereDamage = 1;
+            const float TpSlashDelay = 0.1f;
+            if (IsEnhanced) 
+            {
+                var pos = getNextToEnemyPosition();
+                var rot = getEnemyRotation();
+                if (pos != null)
+                {
+                    TeleportPlayer.Invoke((Vector3)pos, rot);
+                    yield return new WaitForSeconds(TpSlashDelay);
+                }
+            }
 
             animancer.Play(clip).Time = 0;
-            cooldownTo = Time.time + fireCooldown;
-            CreateSphereAttack(SphereRadius, SphereDistance, SphereDamage);
+            CreateSphereAttack(SphereRadius, SphereDistance, Damage);
         }
+
+        Transform getClosestEnemyTransform()
+        {
+            const float SCAN_RADIUS = 20f;
+            var hits = new HashSet<Transform>();
+            Collider[] colliders = new Collider[10];
+            int nrOfHits = Physics.OverlapSphereNonAlloc(transform.position, SCAN_RADIUS, colliders, LayerMask.GetMask("Bots"));
+            if (colliders[0] != null)
+            {
+                var closestEnemyTransform = colliders[0].transform.root;
+                foreach (Collider collider in colliders) 
+                {
+                    var transform = colliders[0].transform.root;
+                    if((transform.position - gameObject.transform.position).magnitude < (closestEnemyTransform.position - gameObject.transform.position).magnitude)
+                        closestEnemyTransform = transform;
+                }
+                return closestEnemyTransform;
+            }
+            return null;
+            
+        }
+
+        Vector3? getNextToEnemyPosition()
+        {
+            var closestEnemyTransform = getClosestEnemyTransform();
+            if (closestEnemyTransform == null) return null;
+
+            var pos = closestEnemyTransform.position + closestEnemyTransform.forward * (-2);
+            return pos;
+        }
+
+        Quaternion? getEnemyRotation()
+        {
+            var closestEnemyTransform = getClosestEnemyTransform();
+            if (closestEnemyTransform == null) return null;
+
+            var rot = closestEnemyTransform.rotation;
+            return rot;
+        }
+
+
+
     }
 }

ABC-practica/dacic-zero-game

Assets/_Project/Scripts/Weapons/LongBow.cs
@@ -1,3 +1,8 @@
+using EventBus;
+using HP;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Weapons
@@ -17,19 +22,87 @@ namespace Weapons
         {
             animancer.Play(clip).Time = 0;
             cooldownTo = Time.time + fireCooldown;
-            ShootArrow(currentCharge);
-            ammo -= 1;
+            if (!IsEnhanced)
+            {
+                ShootArrow(currentCharge);
+                ammo -= 1;
+            }
+            else
+            {
+                const float ArrowSpread = 3f;
+                ShootArrow(currentCharge);
+                ShootArrow(currentCharge, ArrowSpread);
+                ShootArrow(currentCharge, (-1) * ArrowSpread);
+                ammo -= 1;
+            }
         }
 
         protected override void AltFire()
         {
+            StartCoroutine(AltFireAction());
+            cooldownTo = Time.time + fireCooldown;
+        }
+
+        IEnumerator AltFireAction()
+        {
             const float SphereRadius = 2f;
             const float SphereDistance = 0f;
-            const int SphereDamage = 1;
+            const float TpSlashDelay = 0.1f;
+            if (IsEnhanced) 
+            {
+                var pos = getNextToEnemyPosition();
+                var rot = getEnemyRotation();
+                if (pos != null)
+                {
+                    TeleportPlayer.Invoke((Vector3)pos, rot);
+                    yield return new WaitForSeconds(TpSlashDelay);
+                }
+            }
 
             animancer.Play(clip).Time = 0;
-            cooldownTo = Time.time + fireCooldown;
-            CreateSphereAttack(SphereRadius, SphereDistance, SphereDamage);
+            CreateSphereAttack(SphereRadius, SphereDistance, Damage);
         }
+
+        Transform getClosestEnemyTransform()
+        {
+            const float SCAN_RADIUS = 20f;
+            var hits = new HashSet<Transform>();
+            Collider[] colliders = new Collider[10];
+            int nrOfHits = Physics.OverlapSphereNonAlloc(transform.position, SCAN_RADIUS, colliders, LayerMask.GetMask("Bots"));
+            if (colliders[0] != null)
+            {
+                var closestEnemyTransform = colliders[0].transform.root;
+                foreach (Collider collider in colliders) 
+                {
+                    var transform = colliders[0].transform.root;
+                    if((transform.position - gameObject.transform.position).magnitude < (closestEnemyTransform.position - gameObject.transform.position).magnitude)
+                        closestEnemyTransform = transform;
+                }
+                return closestEnemyTransform;
+            }
+            return null;
+            
+        }
+
+        Vector3? getNextToEnemyPosition()
+        {
+            var closestEnemyTransform = getClosestEnemyTransform();
+            if (closestEnemyTransform == null) return null;
+
+            var pos = closestEnemyTransform.position + closestEnemyTransform.forward * (-2);
+            return pos;
+        }
+
+        Quaternion? getEnemyRotation()
+        {
+            var closestEnemyTransform = getClosestEnemyTransform();
+            if (closestEnemyTransform == null) return null;
+
+            var rot = closestEnemyTransform.rotation;
+            return rot;
+        }
+
+
+
     }
 }